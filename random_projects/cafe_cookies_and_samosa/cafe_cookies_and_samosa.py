29eeb2e89ea93207b721328d7e003c6f907f59b7
Pawel <pawel.habas@op.pl>
2026-01-09 10:31:58 +0100

    Cookies and Samosa Cafe - TDD programing

---
+++ random_projects/cafe_cookies_and_samosa/cafe_cookies_and_samosa.py
@@ -0,0 +1,42 @@
+# A local cafe wants a program that suggests a snack.
+# If a customer asks for cookies or samosas, it confirms the order.
+# Otherwise, it says it's not available.
+#
+# Task:
+# - Take snack input
+# - If it's "cookies" or "samosa", confirm the order
+# - Else, show unavailability
+
+import unittest
+import pytest
+from enum import Enum
+
+
+class OrderReplies(Enum):
+    confirmation = 'Order confirmed.'
+    unavailability = 'Product unavailable'
+
+
+def cafe_worker(order: str) -> OrderReplies:
+    return OrderReplies.confirmation if order in ['cookies', 'samosa'] else OrderReplies.unavailability
+
+
+def test_cafe_worker():
+    assert cafe_worker('cookie') == OrderReplies.unavailability, 'cookie Failed'
+    assert cafe_worker('cookies') == OrderReplies.confirmation, 'cookies Failed'
+    assert cafe_worker('samosa') == OrderReplies.confirmation, 'samosa Failed'
+    assert cafe_worker('') == OrderReplies.unavailability, 'empty string Failed'
+    with pytest.raises(TypeError):
+        cafe_worker()
+
+
+class TestCafeWorker(unittest.TestCase):
+    def test_cafe_worker_confirmation(self):
+        self.assertEqual(cafe_worker('cookies'), OrderReplies.confirmation, '"cookies" test Failed')
+        self.assertEqual(cafe_worker('samosa'), OrderReplies.confirmation, '"samosa" test Failed')
+
+    def test_cafe_worker_unavailability(self):
+        self.assertRaises(TypeError, cafe_worker, )
+        self.assertEqual(cafe_worker(''), OrderReplies.unavailability, 'EmptyString test Failed')
+        self.assertEqual(cafe_worker('cookie'), OrderReplies.unavailability, 'Wrong word test Failed')
+        self.assertEqual(cafe_worker('samosas'), OrderReplies.unavailability, 'Wrong word test Failed')
